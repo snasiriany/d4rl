b62fd8e9c7359af012aa3932d73912d3c27a9517
snasiriany <snasiriany@gmail.com>
2021-11-19 10:21:40 -0600

    add check_success func for robomimic logging

--- d4rl/kitchen/kitchen_envs.py
+++ d4rl/kitchen/kitchen_envs.py
@@ -86,6 +86,9 @@ class KitchenBase(KitchenTaskRelaxV1, OfflineEnv):
             done = not self.tasks_to_complete
         return obs, reward, done, env_info
 
+    def _check_success(self):
+        return {'task': (not self.tasks_to_complete)}
+
     # def render(self, mode='human'):
     #     # Disable rendering to speed up environment evaluation.
     #     return []
